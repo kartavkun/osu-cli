dd5c73385b2a00675a21ab506097ee32668249d0
kartavkun <nikitarobezhko@yandex.ru>
2025-08-10 15:43:11 +0500

    add version flag

--- osucli/main.py
+++ osucli/main.py
@@ -5,8 +5,9 @@ from osucli.rs import print_score_data  # на будущее
 def main():
     parser = argparse.ArgumentParser(description="osu! CLI tool")
     parser.add_argument("--fetch", action="store_true", help="Show profile info")
-    parser.add_argument("--rs", action="store_true", help="Show recent score")  # для будущего
+    parser.add_argument("--rs", action="store_true", help="Show recent score")
     parser.add_argument("user", nargs="?", default=None, help="User ID or nickname (optional)")
+    parser.add_argument("-v", "--version", action="version", version=f"v0.2")
 
     args = parser.parse_args()
 
@@ -16,3 +17,6 @@ def main():
         print_score_data()
     else:
         parser.print_help()
+
+if __name__ == "__main__":
+    main()
